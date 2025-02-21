45faa590fa2b385c1d183f7ca9e5ffdcf9341a2f
Mark DiFranco <mpdifranco@icloud.com>
2025-02-21 08:06:31 -0500

    Add key encoding exceptions for additionalProperties key, since it's not snake case

--- Sources/OpenAIKit/RequestHandler/Request.swift
+++ Sources/OpenAIKit/RequestHandler/Request.swift
@@ -35,7 +35,45 @@ extension Request {
 extension JSONEncoder {
     static var requestEncoder: JSONEncoder {
         let encoder = JSONEncoder()
-        encoder.keyEncodingStrategy = .convertToSnakeCase
+        encoder.keyEncodingStrategy = .custom { codingKeys in
+            guard let lastKey = codingKeys.last else { return AnyCodingKey(stringValue: "") }
+
+            guard lastKey.intValue == nil else {
+                return lastKey
+            }
+
+            let keyString = lastKey.stringValue
+
+            // Preserve known camelCase keys
+            let exceptions = ["additionalProperties"]
+            if exceptions.contains(keyString) {
+                return lastKey
+            }
+
+            // Convert to snake_case otherwise
+            let snakeCase = keyString.replacingOccurrences(
+                of: "([a-z])([A-Z])",
+                with: "$1_$2",
+                options: .regularExpression
+            ).lowercased()
+
+            return AnyCodingKey(stringValue: snakeCase)
+        }
         return encoder
+    }
+}
+
+struct AnyCodingKey: CodingKey {
+    var stringValue: String
+    var intValue: Int?
+
+    init(stringValue: String) {
+        self.stringValue = stringValue
+        self.intValue = nil
+    }
+
+    init?(intValue: Int) {
+        self.stringValue = String(intValue)
+        self.intValue = intValue
     }
 }
