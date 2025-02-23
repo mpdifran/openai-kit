4bb89bf840e210009bf63b457707a95c8a43558b
Mark DiFranco <mpdifranco@icloud.com>
2025-02-23 12:36:15 -0500

    Make Chat ID optional

--- Sources/OpenAIKit/Chat/Chat.swift
+++ Sources/OpenAIKit/Chat/Chat.swift
@@ -4,7 +4,7 @@ import Foundation
  Given a prompt, the model will return one or more predicted chat completions, and can also return the probabilities of alternative tokens at each position.
  */
 public struct Chat {
-    public let id: String
+    public let id: String?
     public let object: String
     public let created: Date
     public let model: String
