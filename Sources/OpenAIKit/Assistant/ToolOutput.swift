7b5c1d61a0b189b9fdd4472fa529ac1c0cf69894
Mark DiFranco <mpdifranco@icloud.com>
2025-04-27 17:01:01 -0500

    Update the tool output to match what the API expects

--- Sources/OpenAIKit/Assistant/ToolOutput.swift
+++ Sources/OpenAIKit/Assistant/ToolOutput.swift
@@ -9,30 +9,13 @@ import Foundation
 
 public struct ToolOutput: Codable {
     public let toolCallID: String
-    public let output: String?
+    public let output: String
 
     public init(
         toolCallID: String,
-        output: String?
+        output: String
     ) {
         self.toolCallID = toolCallID
         self.output = output
-    }
-
-    private enum CodingKeys: String, CodingKey {
-        case toolCallID
-        case output
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(toolCallID, forKey: .toolCallID)
-        if let output = output {
-            try container.encode(output, forKey: .output)
-        } else {
-            // Encode an empty object when output is nil
-            let emptyObject: [String: String] = [:]
-            try container.encode(emptyObject, forKey: .output)
-        }
     }
 }
